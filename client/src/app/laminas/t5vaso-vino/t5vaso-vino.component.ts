85efa0e7d9aa35cde1fd08ddca5a6915123589a9
NicoBarti <nicolas.barticevic@gmail.com>
2018-02-27 13:10:48 -0300

    mismo anterior

---
+++ client/src/app/laminas/t5vaso-vino/t5vaso-vino.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-t5vaso-vino',
+  templateUrl: './t5vaso-vino.component.html',
+  styleUrls: ['./t5vaso-vino.component.css']
+})
+export class T5vasoVinoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ client/src/app/laminas/t6piscola/t6piscola.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-t6piscola',
+  templateUrl: './t6piscola.component.html',
+  styleUrls: ['./t6piscola.component.css']
+})
+export class T6piscolaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
