5e963cd9e18770f5b5fca74f92141f1dc802b8ef
NicoBarti <nicolas.barticevic@gmail.com>
2018-02-26 21:36:22 -0300

    todos los tragos pregunta 2

--- client/src/app/auditc/p2/p2.component.ts
+++ client/src/app/auditc/p2/p2.component.ts
@@ -30,12 +30,24 @@ export class P2Component {
   cerveza: number = 0;
   vino: number = 0;
   destilado: number = 0;
+  shop_grande: number = 0;
+  vaso_vino: number = 0;
+  combinado_simple: number = 0;
+  combinado_fuerte: number =0;
+  shop_pequeno: number=0;
+  botella_cerveza: number=0;
+  botella_vino: number=0;
+  caja_vino: number=0;
+  botella_licor: number=0;
 
 
   almacenaPuntaje() {
 this.sumaPuntajeService.setP2(
       this.convertirTragosService.convertir(
-        this.cerveza, this.vino, this.destilado));
+        this.cerveza, this.vino, this.destilado,
+        this.shop_grande, this.vaso_vino, this.combinado_simple,
+        this.combinado_fuerte, this.shop_pequeno, this.botella_cerveza,
+        this.botella_vino, this.caja_vino, this.botella_licor));
     this.router.navigate(['auditc/p3'])
 
   }
